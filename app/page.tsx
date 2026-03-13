d730fddb481bce2ea336aaed8a5c302e5d57ae74
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-13 20:43:29 +0100

    Update wrapper container in current pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <>
       <p>
         👷🏻‍♂️ Please remove this block and show the <strong>most popular recipes</strong> here.
       </p>
@@ -8,6 +8,6 @@ export default function Home() {
         <span>You can use the api endpoint </span>
         <code className="text-sm font-light">/api/recipes/popular</code>.
       </p>
-    </div>
+    </>
   );
 }
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,6 +1,6 @@
 export default async function RecipePage() {
   return (
-    <div>
+    <>
       <p className="block text-radial">
         👷🏻‍♂️ Please remove this block and show <strong>all the recipes</strong> here, paged by 10.
       </p>
@@ -9,6 +9,6 @@ export default async function RecipePage() {
         <span>You can use the api endpoint </span>
         <code className="text-sm font-light">/api/recipes/all</code>.
       </p>
-    </div>
+    </>
   );
 }
