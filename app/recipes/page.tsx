779cbbfa79498a79b54c315a7fa454f9ac88ba1b
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-15 16:59:17 +0100

    Remove redundant recipe page files

--- app/recipes/page.tsx
+++
@@ -1,5 +0,0 @@
-import SectionCocktailOverview from "../components/03_organisms/section-cocktail-overview/section-cocktail-overview";
-
-export default async function RecipePage() {
-  return <SectionCocktailOverview />;
-}
