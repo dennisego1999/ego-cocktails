1bde36f2b49431341c74c8ed5769ecaa0974b429
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-15 17:30:59 +0100

    Convert DTOs to plain objects for client transmission

--- app/cocktails/page.tsx
+++ app/cocktails/page.tsx
@@ -1,13 +1,17 @@
 import CocktailService from "@/app/classes/cocktail/CocktailService";
 import SectionCocktailOverview from "../components/03_organisms/section-cocktail-overview/section-cocktail-overview";
+import CocktailDTO from "../classes/cocktail/CocktailDTO";
 
 export default async function CocktailsPage() {
   // Fetch first page on the server
   const initialData = await CocktailService.instance.getPage();
 
+  // Convert DTOs to plain objects for client transmission
+  const plainCocktails = initialData.cocktails.map((cocktail) => cocktail.toJSON() as CocktailDTO);
+
   return (
     <SectionCocktailOverview
-      initialCocktails={initialData.cocktails}
+      initialCocktails={plainCocktails}
       initialHasNext={initialData.hasNext ?? false}
     />
   );
