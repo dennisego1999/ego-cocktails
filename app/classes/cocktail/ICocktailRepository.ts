45101886eeecdf35c02092f391ebd1cef97e33b1
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-16 18:17:35 +0100

    Update CocktailRepository contract

--- app/classes/cocktail/ICocktailRepository.ts
+++ app/classes/cocktail/ICocktailRepository.ts
@@ -4,7 +4,7 @@ export default interface ICocktailRepository {
   getPage(options?: {
     offset?: number;
     limit?: number;
-  }): Promise<{ cocktails: CocktailDTO[]; hasNext?: boolean }>;
+  }): Promise<{ cocktails: CocktailDTO[]; hasNext: boolean }>;
   getAll(): Promise<CocktailDTO[]>;
   getPopular(limit?: number): Promise<CocktailDTO[]>;
   search(query: string): Promise<CocktailDTO[]>;
