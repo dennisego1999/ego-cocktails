950568e3543fb5eb89c3ec122f4b77a7b2fff62e
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-18 12:31:13 +0100

    Add comment

--- app/lib/client-bootstap.ts
+++ app/lib/client-bootstap.ts
@@ -4,6 +4,7 @@ import CocktailClient from "../classes/cocktail/CocktailClient";
 import CocktailRepository from "../classes/cocktail/CocktailRepository";
 import CocktailService from "../classes/cocktail/CocktailService";
 
+// Bootstrap OOP layers for client components
 const client = new CocktailClient();
 const repo = new CocktailRepository(client);
 CocktailService.init(repo);
